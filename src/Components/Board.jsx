59ad5c6031937dd7cb9318d708cb62f3a5e87723
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-07 22:29:50 -0600

    rewrote and planned the building of the board

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -19,22 +19,35 @@ import styles from "./Board.module.css";
 // }
 
 function Board() {
-  function createGrid() {
-    return Array.from({ length: 9 }).map((_, parentIndex) => (
-      <div key={parentIndex} className={styles.bigSquare}>
-        {Array.from({ length: 9 }).map((_, childIndex) => (
-          <p
-            key={childIndex}
-            className={styles.square}
-            id={`${parentIndex + 1}-${childIndex + 1}`}
-          >
-            {`s${parentIndex + 1}-i${childIndex + 1}`}
-          </p>
+  // function createGrid() {
+  //   return Array.from({ length: 9 }).map((_, parentIndex) => (
+  //     <div key={parentIndex} className={styles.bigSquare}>
+  //       {Array.from({ length: 9 }).map((_, childIndex) => (
+  //         <p
+  //           key={childIndex}
+  //           className={styles.square}
+  //           id={`${parentIndex + 1}-${childIndex + 1}`}
+  //         >
+  //           {`s${parentIndex + 1}-i${childIndex + 1}`}
+  //         </p>
+  //       ))}
+  //     </div>
+  //   ));
+  // }
+
+  //I need to use math to add classes for the large squares and use the indexes to add ids, also need to add classes or data for rows and columns
+  function createGridTwo() {
+    return Array.from({ length: 9 }).map((_, outerIndex) => (
+      <div key={`${outerIndex}-a`}>
+        {Array.from({ length: 9 }).map((_, innerIndex) => (
+          <div
+            key={`${outerIndex}-${innerIndex}`}
+          >{`${outerIndex}-${innerIndex}`}</div>
         ))}
       </div>
     ));
   }
-  return <div className={styles.boardContainer}>{createGrid()}</div>;
+  return <div className={styles.boardContainer}>{createGridTwo()}</div>;
 }
 
 export default Board;
