18624e578929d467838f333317763511a619599f
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-09 11:12:52 -0600

    got the board rebuilt with rows as the main organizing component not the 3x3 sqaures while still maintaining the borders in the correct places

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -18,15 +18,37 @@ import styles from "./Board.module.css";
 //   }
 // }
 
+function assignBorderClass(row, column) {
+  const borderStyles = [];
+  if (row == 0 || row == 3 || row == 6) {
+    borderStyles.push(styles.topBorder);
+  } else if (row == 8) {
+    borderStyles.push(styles.bottomBorder);
+  }
+  if (column == 0 || column == 3 || column == 6) {
+    borderStyles.push(styles.leftBorder);
+  } else if (column == 8) {
+    borderStyles.push(styles.rightBorder);
+  }
+  return borderStyles.join(" ");
+}
+
 function Board() {
-  //I need to use math to add classes for the large squares and use the indexes to add ids, also need to add classes or data for rows and columns
   function createGrid() {
     return Array.from({ length: 9 }).map((_, outerIndex) => (
-      <div key={`${outerIndex}-a`} className={styles[`row${outerIndex}`]}>
+      <div
+        key={`${outerIndex}-a`}
+        className={`${styles.row} ${`row${outerIndex}`}`}
+      >
         {Array.from({ length: 9 }).map((_, innerIndex) => (
           <div
             key={`${outerIndex}-${innerIndex}`}
-            className={styles[`column${innerIndex}`]}
+            className={` ${
+              styles.square
+            } ${`column${innerIndex}`} ${assignBorderClass(
+              outerIndex,
+              innerIndex
+            )}`}
           >{`${outerIndex}-${innerIndex}`}</div>
         ))}
       </div>
