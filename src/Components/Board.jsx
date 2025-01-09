0ec116038e404b03960b9d101d38749335613471
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-09 11:57:53 -0600

    copied down chats fillboard function

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,22 +1,69 @@
 import styles from "./Board.module.css";
 
-// function checkIsValid(board, row, col, num) {
-//   for (let i = 0; i < 9; i++) {
-//     if (board[row][i] === num || board[i][col]) {
-//       return false;
-//     }
-//   }
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-//   const startRow = Math.floor(row / 3) * 3;
-//   const startCol = Math.floor(col / 3) * 3;
-//   for (let i = startRow; i < startRow + 3; i++) {
-//     for (let j = startCol; j < startCol + 3; j++) {
-//       if (board[i][j] === num) {
-//         return false;
-//       }
-//     }
-//   }
-// }
+function fillBoard(board) {
+  function findEmptyCell(board) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === 0) {
+          return { row, col };
+        }
+      }
+    }
+    return null;
+  }
+  function solve(board) {
+    const emptyCell = findEmptyCell(board);
+    if (!emptyCell) {
+      return true;
+    }
+
+    const { row, col } = emptyCell;
+
+    for (let num = 1; num <= 9; num++) {
+      if (checkIsValid(board, row, col, num)) {
+        board[row][col] = num;
+
+        if (solve(board)) {
+          return true;
+        }
+        board[row][col] = 0;
+      }
+    }
+    return false;
+  }
+  solve(board);
+}
+
+function checkIsValid(board, row, col, num) {
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] === num || board[i][col] === num) {
+      return false;
+    }
+  }
+
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
+      if (board[i][j] === num) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
 function assignBorderClass(row, column) {
   const borderStyles = [];
@@ -49,12 +96,18 @@ function Board() {
               outerIndex,
               innerIndex
             )}`}
-          >{`${outerIndex}-${innerIndex}`}</div>
+          ></div>
         ))}
       </div>
     ));
   }
-  return <div className={styles.boardContainer}>{createGrid()}</div>;
+  return (
+    <div className={styles.boardContainer}>
+      {createGrid()}
+      <button onClick={() => fillBoard(board)}>hey</button>
+      <button onClick={() => console.log(board, "hey")}>Ho</button>
+    </div>
+  );
 }
 
 export default Board;
